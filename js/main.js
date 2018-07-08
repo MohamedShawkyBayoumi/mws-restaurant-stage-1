206b24b8ecb4e3ac6ca5d14dfc7581aeb3991336
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2018-07-08 11:13:17 +0200

    edit some pathes into cacheAll

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js').then(function(){
+  navigator.serviceWorker.register('./sw.js').then(function(){
       console.log('Registration Worked!');
   }).catch(function(){
       console.log('Registration Failed');
